c0285c11c592b9db748866463d335236ad42012f
Kristina Olsson <kristinaolsson10@gmail.com>
2019-02-17 16:28:43 +0100

    Added JSX way of printing hello world element

--- src/index.js
+++ src/index.js
@@ -5,3 +5,9 @@ const helloElement = React.createElement("div", {className: "hello-react"}, "Hel
 //const helloElement = React.createElement("div", {className: "hello-react", children: [<a href="http://www.google.com">Hello my child!</a>]});
 //console.log(helloElement);
 ReactDOM.render(helloElement, document.getElementById("react-root"));
+
+/* JSX with interpolation */
+const helloJsxText = "Hello! My name is JSX. Just JSX!";
+const showText = false;
+const helloJsx = <div className="jsx">{showText ? helloJsxText : "Something else"}</div>
+ReactDOM.render(helloJsx, document.getElementById("react-jsx-root"));
