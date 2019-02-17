ffe1fa5f71bfc25cab3c9bc5d39fd8a29a5a9d05
Kristina Olsson <kristinaolsson10@gmail.com>
2019-02-17 14:42:08 +0100

    Step 2 - create element using react api

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./App";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const helloElement = React.createElement("div", {className: "hello-react"}, "Hello, my friend! I am React!");
+//const helloElement = React.createElement("div", {className: "hello-react", children: [<a href="http://www.google.com">Hello my child!</a>]});
+//console.log(helloElement);
+ReactDOM.render(helloElement, document.getElementById("react-root"));
