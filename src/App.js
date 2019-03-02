4922c4b586689dd15e7e56d7860ecfe38e006f83
Kristina Olsson <kristina.olsson@maersk.com>
2019-03-02 20:17:46 +0100

    Add components with props and state

--- src/App.js
+++ src/App.js
@@ -1,5 +1,35 @@
 import React from "react";
 
-const App = () => <h1>Hello world</h1>
+/* class component */
+/* class components need to extend React.Component */
+class AppWithProp extends React.Component {
+    /* needs to have a render fuction which returns on one node */
+    render() {
+        return <h1>Hello {this.props.name === "Kristina" ? this.props.name : "I don't care!"}</h1>
+    }
+}
 
-export default App;
+
+class AppWithState extends React.Component {
+    constructor() {
+        super()
+        this.state = {
+            text: "This is the default state"
+        }
+    }
+    update = (event) => {
+        this.setState({text: event.target.value})
+    }
+    render() {
+        return <div>
+            <input type="text" onChange={this.update}/>
+             <h1>{this.state.text}</h1>
+         </div>
+    }
+}
+
+/* functional component */
+/* Here we create a const variable which is equal to a function, which simply returns our jsx */
+//const App = () => <h1>Hello component!</h1>
+
+export default AppWithState;
--- src/index.js
+++ src/index.js
@@ -1,13 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import App from "./App"
 
-const helloElement = React.createElement("div", {className: "hello-react"}, "Hello, my friend! I am React!");
-//const helloElement = React.createElement("div", {className: "hello-react", children: [<a href="http://www.google.com">Hello my child!</a>]});
+const helloElement = React.createElement("h1", {className: "hello-react"}, "Hello, my friend! I am React!");
+//const helloElement = React.createElement("h1", {className: "hello-react", children: [<a href="http://www.google.com">Hello my child!</a>]});
 //console.log(helloElement);
 ReactDOM.render(helloElement, document.getElementById("react-root"));
 
 /* JSX with interpolation */
 const helloJsxText = "Hello! My name is JSX. Just JSX!";
 const showText = false;
-const helloJsx = <div className="jsx">{showText ? helloJsxText : "Something else"}</div>
+const helloJsx = <h1 className="jsx">{showText ? helloJsxText : "Something else"}</h1>
 ReactDOM.render(helloJsx, document.getElementById("react-jsx-root"));
+
+/* Hello world with React component */
+ReactDOM.render(<App name="Hannah" />, document.getElementById("react-component-root"))
